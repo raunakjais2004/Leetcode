e2ed554742cdf21f63029b23882d5f0fd87601de
Raunak Jaiswal <148192234+raunakjais2004@users.noreply.github.com>
2026-08-16 12:43:01 +0530

    Time: 3 ms (94.03%), Space: 46.6 MB (25.79%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+        if (s.length() != t.length()) { 
+            return false;
+            }
+        char [] a1=s.toCharArray();
+        char [] a2=t.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+
+        return Arrays.equals(a1, a2);
+        
+    }
+}
