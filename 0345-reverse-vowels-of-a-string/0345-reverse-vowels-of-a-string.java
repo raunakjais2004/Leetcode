db3840e043c4e6e03e430653246b1500b6b5df8f
Raunak Jaiswal <148192234+raunakjais2004@users.noreply.github.com>
2026-08-16 14:29:17 +0530

    Time: 2 ms (99.34%), Space: 46.6 MB (65.06%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String reverseVowels(String s) {
+
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // Left vowel nahi hai
+            if (arr[left] != 'a' && arr[left] != 'e' &&
+                arr[left] != 'i' && arr[left] != 'o' &&
+                arr[left] != 'u' &&
+                arr[left] != 'A' && arr[left] != 'E' &&
+                arr[left] != 'I' && arr[left] != 'O' &&
+                arr[left] != 'U') {
+
+                left++;
+            }
+
+            // Right vowel nahi hai
+            else if (arr[right] != 'a' && arr[right] != 'e' &&
+                     arr[right] != 'i' && arr[right] != 'o' &&
+                     arr[right] != 'u' &&
+                     arr[right] != 'A' && arr[right] != 'E' &&
+                     arr[right] != 'I' && arr[right] != 'O' &&
+                     arr[right] != 'U') {
+
+                right--;
+            }
+
+            // Dono vowel hain
+            else {
+
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return new String(arr);
+    }
+}
