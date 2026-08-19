6de68e31295a70539fae1f0e5595025fb3d10ff8
Raunak Jaiswal <148192234+raunakjais2004@users.noreply.github.com>
2026-08-19 21:14:23 +0530

    Time: 1 ms (89.45%), Space: 43 MB (78.83%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -26,3 +26,36 @@ class Solution {
         return false;
     }
 }
+
+// class Solution {
+//     public boolean detectCapitalUse(String word) {
+
+//         int upper = 0;
+//         int lower = 0;
+
+//         for (int i = 0; i < word.length(); i++) {
+
+//             char ch = word.charAt(i);
+
+//             if (Character.isUpperCase(ch)) {
+//                 upper++;
+//             } else {
+//                 lower++;
+//             }
+//         }
+
+//         if (upper == word.length()) {
+//             return true;
+//         }
+
+//         if (lower == word.length()) {
+//             return true;
+//         }
+
+//         if (upper == 1 && Character.isUpperCase(word.charAt(0))) {
+//             return true;
+//         }
+
+//         return false;
+//     }
+// }
