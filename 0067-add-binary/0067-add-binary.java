99825750054c38e901044d3764a2440707278be1
Raunak Jaiswal <148192234+raunakjais2004@users.noreply.github.com>
2026-08-19 11:07:37 +0530

    Time: 1 ms (99.94%), Space: 43.1 MB (98.39%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,22 +1,26 @@
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder ans = new StringBuilder();
-        int carry=0;
-        int i=a.length()-1;
-        int j=b.length()-1;
 
-        while(i>=0 || j>=0 || carry!=0) {
+        StringBuilder ans = new StringBuilder();
+
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+
             int dA = 0;
             int dB = 0;
 
             if (i >= 0) {
                 dA = a.charAt(i) - '0';
             }
+
             if (j >= 0) {
                 dB = b.charAt(j) - '0';
             }
 
-            int sum=dA+dB+carry;
+            int sum = dA + dB + carry;
 
             int digit = sum % 2;
             carry = sum / 2;
@@ -25,9 +29,8 @@ class Solution {
 
             i--;
             j--;
-
         }
-        return ans.reverse().toString();
 
+        return ans.reverse().toString();
     }
 }
